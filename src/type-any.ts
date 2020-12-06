38afad6774989e3392aea4e8ed5d191886765e9a
pedro <pedromf34@outlook.com>
2020-12-06 15:25:52 -0300

    Tipo any

---
+++ src/type-any.ts
@@ -0,0 +1,8 @@
+// utilize apenas em ultimo caso
+function showMessage(msg: any) {
+  return msg;
+}
+
+console.log(showMessage([1, 2, 3]));
+console.log(showMessage("olá"));
+console.log(showMessage(1));
