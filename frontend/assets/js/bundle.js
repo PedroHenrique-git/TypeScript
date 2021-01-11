08bfa59ec19eda4c855b9f6be8139dff440e8d04
pedro <pedromf34@outlook.com>
2021-01-11 20:23:02 -0300

    frontend typescript

---
+++ frontend/assets/js/bundle.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/*!***********************************!*\
+  !*** ./src/testeWebpack/index.ts ***!
+  \***********************************/
+
+
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
