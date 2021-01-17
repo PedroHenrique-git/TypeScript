e5401d178b0da893197adb54e30e6b2828be6b11
pedro <pedromf34@outlook.com>
2021-01-17 15:33:37 -0300

    type alias implementation in class

---
+++ src/POO/type-alias-implementation-in-class.ts
@@ -0,0 +1,15 @@
+type TipoPessoa = {
+  nome: string;
+  sobrenome: string;
+  nomeCompleto(): string;
+};
+
+export class Pessoa implements TipoPessoa {
+  constructor(public nome: string, public sobrenome: string) {}
+  nomeCompleto(): string {
+    return `${this.nome} ${this.sobrenome}`;
+  }
+}
+
+const pessoa = new Pessoa("Luiz", "Miranda");
+console.log(pessoa.nomeCompleto());
