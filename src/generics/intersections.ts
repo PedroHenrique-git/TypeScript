6a2c6f224ab22c8cc60f6588c5d9491e1f66e6ac
pedro <pedromf34@outlook.com>
2021-01-27 20:55:26 -0300

    generics intersection

---
+++ src/generics/intersections.ts
@@ -0,0 +1,15 @@
+function unirObjetos<o1, o2>(obj1: o1, obj2: o2): o1 & o2 {
+  return { ...obj1, ...obj2 };
+}
+
+function unirObjetos2<o1, o2>(obj1: o1, obj2: o2): o1 & o2 {
+  return Object.assign({}, obj1, obj2);
+}
+
+const obj1 = { chave1: "valor1" };
+const obj2 = { chave2: "valor2" };
+const uniao = unirObjetos(obj1, obj2);
+const uniao2 = unirObjetos2(obj1, obj2);
+
+console.log(uniao);
+console.log(uniao2);
