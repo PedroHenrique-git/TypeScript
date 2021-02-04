fb2196c9592d65f63fc88d61d9c9eac53c70b14c
pedro <pedromf34@outlook.com>
2021-02-03 21:55:17 -0300

    decorators parameters class

--- src/Decorators/decorators-parameters.ts
+++ src/Decorators/decorators-parameters.ts
@@ -1,22 +1,30 @@
-function inverteNomeECor<T extends new (...args: any[]) => any>(target: T): T {
-  console.log("Sou o decorator e recebi", target);
-  return class extends target {
-    private cor: string;
-    private nome: string;
+function inverteNomeECor(param1: string, param2: string) {
+  return <T extends new (...args: any[]) => any>(target: T): T => {
+    console.log("Sou o decorator e recebi", target);
+    return class extends target {
+      private cor: string;
+      private nome: string;
 
-    constructor(...args: any[]) {
-      super(...args);
-      this.nome = this.reverseString(args[0]);
-      this.cor = this.reverseString(args[1]);
-    }
+      constructor(...args: any[]) {
+        super(...args);
+        this.nome = this.reverseString(args[0]);
+        this.cor = this.reverseString(args[1]);
+      }
 
-    reverseString(stringReceived: string): string {
-      return stringReceived.split("").reverse().join("");
-    }
+      reverseString(stringReceived: string): string {
+        return (
+          stringReceived.split("").reverse().join("") +
+          param1 +
+          " " +
+          " " +
+          param2
+        );
+      }
+    };
   };
 }
 
-@inverteNomeECor
+@inverteNomeECor("Valor1", "Valor2")
 export class Animal {
   constructor(public nome: string, public cor: string) {
     console.log("Sou a classe");
